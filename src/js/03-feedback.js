9e175cd1c52e8d058c72bce8584b822f4d5aed83
Ann <ania.somko@gmail.com>
2022-06-26 12:56:59 +0300

    change task #3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,10 +2,9 @@ import throttle from 'lodash.throttle';
 import '../css/03-feedback.css';
 
 const form = document.querySelector('.feedback-form');
-const mail = document.querySelector('input');
-const text = document.querySelector('textarea');
+
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+let formData = {};
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));
@@ -20,15 +19,23 @@ function onFormSubmit(e) {
 }
 
 function onFormInput(e) {
-  formData[e.target.name] = e.target.value;
+  formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function formOutput(e) {
   let saveData = localStorage.getItem(STORAGE_KEY);
-  if (saveData) {
-    saveData = JSON.parse(saveData);
-    mail.value = saveData.email;
-    text.value = saveData.message;
+  try {
+    if (saveData) {
+      saveData = JSON.parse(saveData);
+      form.elements.email.value = saveData.email;
+      form.elements.message.value = saveData.message;
+    }
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
   }
 }
